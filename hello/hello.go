ae73eeb6f0dc7fd9f490c0fab8a12c46537fb832
Joao Wictor <joaowictoroliveira3301@gmail.com>
2025-04-17 21:18:37 -0300

    adiciona leitura de sites de um arquivo e implementa tratamento de erros

--- hello/hello.go
+++ hello/hello.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -101,7 +104,22 @@ func leSitesDoArquivo() []string {
 	if err != nil {
 		fmt.Println("Ocorreu um erro:", err)
 	}
-	fmt.Println(arquivo)
+
+	leitor := bufio.NewReader(arquivo)
+
+	for {
+		linha, err := leitor.ReadString('\n')
+		linha = strings.TrimSpace(linha)
+
+		sites = append(sites, linha)
+
+		if err == io.EOF {
+			break
+		}
+
+	}
+
+	arquivo.Close()
 
 	return sites
 }
